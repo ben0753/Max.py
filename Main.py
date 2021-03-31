6478b5c57caa0ed1ae48a543e7d73fba027ff515
ben0753 <78950358+ben0753@users.noreply.github.com>
2021-03-31 12:01:13 +0530

    Main.py
    This is the main code for our Max

---
+++ Main.py
@@ -0,0 +1,18 @@
+import discord
+bot = discord.Client()
+
+@bot.event
+async def on_ready():
+	guild_count = 0
+	for guild in bot.guilds:
+		print(f"- {guild.id} (name: {guild.name})")
+		guild_count = guild_count + 1
+	print("Max_py is in " + str(guild_count) + " guilds.")
+
+@bot.event
+async def on_message(message):
+	if message.content == "/hello":
+		await message.channel.send("Hey There I Am Your Python Personal Assistant")
+    
+#Use Bot Token
+bot.run("ODI2MDU0MzkwMjYzNTc4NjM0.YGG5GQ.SSaEAiJ4v5MHHTMspwHgSqgBdQE")
