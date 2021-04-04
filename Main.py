53f781330a5f2f73cea1ba0039562b5b665275f2
ben0753 <78950358+ben0753@users.noreply.github.com>
2021-04-04 17:10:53 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,10 +9,20 @@ async def on_ready():
 		guild_count = guild_count + 1
 	print("Max_py is in " + str(guild_count) + " guilds.")
 
-@bot.event
+
+@bot.event 
 async def on_message(message):
-	if message.content == "/hello":
-		await message.channel.send("Hey There I Am Your Python Personal Assistant")
-    
+    if message.content == "/about":
+        await message.channel.send(" :thumbsup: This Bot Is In Devlopment Actually Created By Ben.py #5021 ")
+
+    elif message.content == "/hello":
+		    await message.channel.send(" :v: Hey There I Am Your Python Personal Assistant")
+
+    elif message.content == "/what is python":
+		    await message.channel.send(" :book: Python is an interpreted, high-level and general-purpose programming language. Python's design philosophy emphasizes code readability with its notable use of significant indentation.")
+
+    elif message.content == "/what is if else statement":
+		    await message.channel.send(" :book: ")    
 #Use Bot Token
-bot.run("your_bot_token_not_mine")
+bot.run("Use_your_own")
+
